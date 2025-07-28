07d800a0993e5bcf8efecdb0a9f427303eba5a15
Yohan Kavishka <sdykjayathilaka@students.nsbm.ac.lk>
2025-07-29 00:44:45 +0530

    feat(index)kit

--- food-ecommerce-app/src/pages/Home.js
+++ food-ecommerce-app/src/pages/Home.js
@@ -15,6 +15,40 @@ function Home () {
     const toggleMenu = () => setShowMenu (prev => !prev);
 
     return (
-        
+        <div className="home-page">
+            {/* Top Navigation*/}
+            <header cassName="navbar">
+                <div className="logo">FoodZone</div>
+                <FaBars className="menu-icon" onClick={toggleMenu} />
+                <div className="nav-icons">
+                    <FaHome title="Home" />
+                    <FaHamburger title="Burgers" />
+                    <FaShoppingCart title="Cart"  />
+                    <FaPhone title="Contact" />
+                </div>
+            </header>
+
+            {/* side Menu */}
+            <motoin.div 
+                className="side-menu"
+                initial={{x: -300}}
+                animate={{x: showMenu ? 0: -300}}
+                transsition={{ during: 0.4}}
+            >
+                {["Pasta", "Chicken Nuggets", "Taco", "Hotdog", "Pizza", "Sushi", "Burger", "Fries"].map((item, index) => (
+                <motoin.div
+                    key={index}
+                    className="menu-item"
+                    whileHover={{ scale: 1.05 }}
+                    transition={{ type: "spring", stiffness: 300}}
+                >
+                    {item}
+                </motoin.div>
+                ))}
+            </motoin.div>
+
+            {/*Trending Section*/}
+            
+        </div>
     )
 }
